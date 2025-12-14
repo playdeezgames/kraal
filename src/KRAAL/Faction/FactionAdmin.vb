dbc388e20684004c67f142080997dba98db119f5
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-12-14 04:19:23 -0600

    faction admin

---
+++ src/KRAAL/Faction/FactionAdmin.vb
@@ -0,0 +1,19 @@
+﻿Imports KRAAL.Domain
+Imports Spectre.Console
+
+Friend Module FactionAdmin
+    Friend Sub Run(faction As IFaction, quitParent As Action)
+        Menu.Run("Go Back", Sub(choices, quit)
+                                AnsiConsole.MarkupLine($"Faction Id: {faction.FactionId}")
+                                AnsiConsole.MarkupLine($"Faction Name: {faction.FactionName}")
+                                choices.Add(New Choice("Rename Faction...", Sub() FactionRename.Run(faction)))
+                                choices.Add(New Choice("Remove Faction", Sub()
+                                                                             If Choice.Confirm("[red]Are you sure you want to remove this faction?[/]") Then
+                                                                                 faction.Remove()
+                                                                                 quit()
+                                                                                 quitParent()
+                                                                             End If
+                                                                         End Sub))
+                            End Sub)
+    End Sub
+End Module
--- src/KRAAL/Faction/FactionDetail.vb
+++ src/KRAAL/Faction/FactionDetail.vb
@@ -4,22 +4,15 @@ Imports Spectre.Console
 Friend Module FactionDetail
     Friend Sub Run(faction As IFaction)
         Menu.Run("Go Back", Sub(choices, quit)
-
                                 AnsiConsole.MarkupLine($"Faction Id: {faction.FactionId}")
                                 AnsiConsole.MarkupLine($"Faction Name: {faction.FactionName}")
-                                choices.Add(New Choice("Remove Faction", Sub()
-                                                                             If Choice.Confirm("[red]Are you sure you want to remove this faction?[/]") Then
-                                                                                 faction.Remove()
-                                                                                 quit()
-                                                                             End If
-                                                                         End Sub))
-                                choices.Add(New Choice("Rename Faction...", Sub() FactionRename.Run(faction)))
                                 If faction.UnitCount > 0 Then
                                     choices.Add(New Choice("Units...", Sub() FactionUnitList.Run(faction)))
                                 End If
                                 If faction.BuildingCount > 0 Then
                                     choices.Add(New Choice("Buildings...", Sub() FactionBuildingList.Run(faction)))
                                 End If
+                                choices.Add(New Choice("Faction Admin...", Sub() FactionAdmin.Run(faction, quit)))
                             End Sub)
     End Sub
 End Module
